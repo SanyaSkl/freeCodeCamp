3fb6de16b3ad66103d54cadab67fd27e2b7cf976
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-01-23 14:31:31 +0300

    Uninitialized Variables.js

---
+++ Basic-JavaScript/Uninitialized Variables.js
@@ -0,0 +1,15 @@
+/*
+Initialize the three variables a, b, and c with 5, 10,
+    and "I am a" respectively so that they will not be undefined.*/
+
+// Only change code below this line
+let a;
+let b;
+let c;
+// Only change code above this line
+a = 5;
+b = 10;
+c = "I am a"
+a = a + 1;
+b = b + 5;
+c = c + " String!";
