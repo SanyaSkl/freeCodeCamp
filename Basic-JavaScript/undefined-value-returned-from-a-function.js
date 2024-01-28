811451bd395629deb74a17c9d5feb487a070def4
SanyaSkl <90842468+SanyaSkl@users.noreply.github.com>
2024-01-28 19:06:15 +0300

    undefined-value-returned-from-a-function.js

---
+++ Basic-JavaScript/undefined-value-returned-from-a-function.js
@@ -0,0 +1,16 @@
+// Setup
+let sum = 5;
+
+function addThree() {
+    sum = sum + 3;
+}
+
+// Only change code below this line
+function addFive() {
+    let sum = sum + 5 - 2
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
